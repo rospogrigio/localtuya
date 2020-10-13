e48bda18ff3107e362bf2128bdfc7b387b1df24a
Thomas Davis <tdavis@nersc.gov>
2020-10-12 23:01:59 -0700

    This changes the fan component from using a hard coded dp for the fan speed.  The correct dp for fan speed is passed in when the component is referenced.  It also attempts to deal with fans that return 'slow', 'medium' and 'high' for fan speeds, instead of a number.
    Fan controllers with more than 3 speeds are not supported in Home Assistant at this time; they
    will simply have 3 speeds and no more.  There is a discussion of changing this to a percentage
    and letting the component map to the proper setting.
    
    Fan controllers with an 'auto' setting is also not really supported. I simply mapped it to
    the 'medium' speed.
    
    This includes an attempt to deal with oscillate setting stack dumping; it is still hardcoded to dps 8, where
    now if dp 8 is not defined, it ignores it; on one of my fans, 8 is valid, but it's not the oscillate setting.
    
    This also includes a fix for when a sensor value is text.
    
    I am using this with  air purifier and a ceiling fan controller with dimmer.
    
    The ceiling fan controller is the Treatlife Single Pole WiFi light switch/Fan speed controller.
    The air purifier is a RENPHO Air Purifier.

--- custom_components/localtuya/fan.py
+++ custom_components/localtuya/fan.py
@@ -38,6 +38,7 @@ class LocaltuyaFan(LocalTuyaEntity, FanEntity):
         self._is_on = False
         self._speed = SPEED_OFF
         self._oscillating = False
+        self._type = 0
 
     @property
     def oscillating(self):
@@ -73,16 +74,23 @@ class LocaltuyaFan(LocalTuyaEntity, FanEntity):
         self.schedule_update_ha_state()
 
     def set_speed(self, speed: str) -> None:
+        mappings = {
+           SPEED_LOW: [ 1, "low" ],
+           SPEED_MEDIUM: [ 2 , "medium" ],
+           SPEED_HIGH: [ 3, "high" ],
+           "auto": [ 2, "medium" ],
+        }
+
+        dps_id = "%s" % self._dps_id
+
         """Set the speed of the fan."""
         self._speed = speed
+
         if speed == SPEED_OFF:
             self._device.set_dps(False, "1")
-        elif speed == SPEED_LOW:
-            self._device.set_dps("1", "2")
-        elif speed == SPEED_MEDIUM:
-            self._device.set_dps("2", "2")
-        elif speed == SPEED_HIGH:
-            self._device.set_dps("3", "2")
+        else:
+            self._device.set_dps(mappings[speed][self._type - 1], dps_id)
+
         self.schedule_update_ha_state()
 
     def oscillate(self, oscillating: bool) -> None:
@@ -93,21 +101,33 @@ class LocaltuyaFan(LocalTuyaEntity, FanEntity):
 
     @property
     def supported_features(self) -> int:
+        print("%s fan has dps 8: %s" % ( self.name, self.dps(8)))
+        print("%s fan has dps 9: %s" % ( self.name, self.dps(9)))
         """Flag supported features."""
         return SUPPORT_SET_SPEED | SUPPORT_OSCILLATE
 
     def status_updated(self):
+        mappings = {
+          "1": [ SPEED_LOW, 1 ],
+          "2": [ SPEED_MEDIUM, 1 ],
+          "3": [ SPEED_HIGH, 1 ],
+          "4": [ SPEED_HIGH, 1 ],
+          "low": [ SPEED_LOW, 2 ],
+          "medium": [ SPEED_MEDIUM, 2 ],
+          "high": [ SPEED_HIGH, 2 ],
+          "auto": [ SPEED_LOW, 2 ],
+        }
+
+        dps_id = "%s" % self._dps_id
+
         """Get state of Tuya fan."""
         self._is_on = self._status["dps"]["1"]
-        if not self._status["dps"]["1"]:
-            self._speed = SPEED_OFF
-        elif self._status["dps"]["2"] == "1":
-            self._speed = SPEED_LOW
-        elif self._status["dps"]["2"] == "2":
-            self._speed = SPEED_MEDIUM
-        elif self._status["dps"]["2"] == "3":
-            self._speed = SPEED_HIGH
-        self._oscillating = self._status["dps"]["8"]
+
+        self._speed = mappings[self._status["dps"][dps_id]][0]
+        self._type = mappings[self._status["dps"][dps_id]][1]
+
+        if hasattr(self._status["dps"], "8"):
+             self._oscillating = self._status["dps"]["8"]
 
 
 async_setup_entry = partial(async_setup_entry, DOMAIN, LocaltuyaFan, flow_schema)
