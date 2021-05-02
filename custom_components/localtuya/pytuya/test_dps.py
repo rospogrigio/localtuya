8d04667224bc2c8f82e98707b133facddd244faa
Sándor Balázs <balazs.sandor@vcc.live>
2021-05-02 12:08:36 +0200

    decode message from ZMAi-90 Energy Meter

---
+++ custom_components/localtuya/pytuya/test_dps.py
@@ -0,0 +1,20 @@
+import pytest
+from . import TuyaProtocol
+
+dev_id = "asdfghjk"
+local_key = "0123456789012345"
+msg1 = '{ "dps": { "1": 5407, "10": 0, "16": true, "18": "111111111111" } }'
+msg2 = '{ "dps": { "6": "CNEAJ5AACOw=" }, "t": 1613651573 }'
+
+def listener():
+    pass
+
+@pytest.mark.asyncio
+async def test_dps_decode():
+    proto = TuyaProtocol(dev_id, local_key, "3.3", None, listener)
+
+    res = proto._decode_payload(bytes(msg1, "utf-8"))
+    assert res['dps'] == {'1': 5407, '10': 0, '16': True, '18': '111111111111'}
+
+    res = proto._decode_payload(bytes(msg2, "utf-8"))
+    assert res['dps'] == {'6V': 2257, '6A': 10128, '6W': 22840}
